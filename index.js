03cc61275db1e94b3883aeee6509f04b31256cf7
taware8043 <rtaware@qualys.com>
2022-04-24 11:01:00 +0530

    index 7

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App V6 for npm build stagegit')
+  res.send('CICD App V7 for npm build stagegit')
 })
 
 app.get('/status', (req, res) => {
